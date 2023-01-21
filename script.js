0ef7d09623ac57402540b22274d5aeacc479fd49
Mateus Ceni <98861755+Ceni82@users.noreply.github.com>
2023-01-21 15:06:13 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -6,31 +6,23 @@ button.addEventListener("click", add)
 form.addEventListener("change", save)
 
 function add(){ 
-    const today = new Date().toLocaleDateString('pt-br').slice(0,-5)
-    const dayExists = nlwSetup.dayExists (today)
-
+    const today = new Date().toLocaleDateString("pt-br").slice(0, -5)
+    const dayExists = nlwSetup.dayExists(today)
+  
 
     if (dayExists){
          alert('Dia já incluso')
-      return
+         return
     }
         
-    alert('Adicionado com Sucesso')
+    alert('Adicionado com Sucesso ✅')
     nlwSetup.addDay(today)
 }
 
 function save(){
-    localStorage.setItem("NLWSetup@habits",JSON.stringify
-    (nlwSetup.data))
+    localStorage.setItem("NLWSetup@habits", JSON.stringify(nlwSetup.data))
 }
 
-
-
-//const data = {
-//    run:['01-01', '01-02', '01-06', '01-07', '01-08'],
-//    takePills: ['01-03'],
-//    journal: ['01-02']
-//}
-const data = JSON.parse(localStorage.getItem("NlWSetup@habits")) || {}
+const data = JSON.parse(localStorage.getItem("NLWSetup@habits")) || {}
 nlwSetup.setData(data)
 nlwSetup.load()
